e856ad1dba6563647ecff7462f95f2e2c7372fda
nicov51 <nicolas.viennot@laposte.net>
2025-01-17 21:21:39 +0100

    🌱 Add searchbar
    add searchbarComponent and first icone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchbarComponent } from './components/layout/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
