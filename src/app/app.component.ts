fc5fd3526065c2c8e0ae0c58432830fcf6550489
nicov51 <nicolas.viennot@laposte.net>
2025-01-18 16:13:48 +0100

    🌱 Add footerComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchbarComponent } from './components/layout/searchbar/searchbar.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchbarComponent],
+  imports: [RouterOutlet, SearchbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
