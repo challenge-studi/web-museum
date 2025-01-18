5e31501b72697240f005416f96909ab1622cc635
nicov51 <nicolas.viennot@laposte.net>
2025-01-18 16:13:48 +0100

    🌱 Layout
    add layoutcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SearchbarComponent } from './components/layout/searchbar/searchbar.component';
+import { LayoutComponent } from './components/layout/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchbarComponent],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
