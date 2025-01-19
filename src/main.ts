440cfb0930888d8099cefa271755a619cea02c6b
nicov51 <nicolas.viennot@laposte.net>
2025-01-19 16:14:02 +0100

    🌱 Add template home, billetterie, presentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import { routes } from './app/app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+
+const appConfig = {
+  providers: [provideRouter(routes)],
+};
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
