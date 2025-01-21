e2746caf9844645e121c710b445f6276c5b43bb4
bru hem <bruno.hemon131exit>
2025-01-22 00:34:50 +0100

    💄 Creation cards

---
+++ src/app/components/ux/cards/cards.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  imports: [],
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.css',
+})
+export class CardsComponent {}
