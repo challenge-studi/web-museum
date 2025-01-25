5a8d1d644ce434bddf615611d8b4045ed65e5138
bhhdev <bhh.develop@gmail.com>
2025-01-25 19:05:51 +0100

    💄 Change card and Create details-card folder

---
+++ src/app/components/ux/details-card/details-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-card',
+  imports: [],
+  templateUrl: './details-card.component.html',
+  styleUrl: './details-card.component.css',
+})
+export class DetailsCardComponent {}
