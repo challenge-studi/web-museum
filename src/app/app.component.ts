75c1382fb0b32aeeee31ef7bcc04e34d3074d173
nicov51 <nicolas.viennot@laposte.net>
2025-01-26 23:09:35 +0100

    ✅ Pass test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { SearchbarComponent } from './components/layout/searchbar/searchbar.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchbarComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchbarComponent,
+    FooterComponent,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
