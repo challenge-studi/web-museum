10deaf701ef042be714f2b5b4704928ed934bb74
yamina <benhiziya7@gmail.com>
2025-01-29 15:18:26 +0100

    💄 Make ticket

---
+++ src/app/components/billet/billet.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billet',
+  imports: [],
+  templateUrl: './billet.component.html',
+  styleUrl: './billet.component.css',
+})
+export class BilletComponent {}
