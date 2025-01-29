dcf96c78c25392d29b8174b2476b857b13668751
Rémi PONCHE <ponche62880@gmail.com>
2025-01-29 20:38:04 +0100

    🐛 Remove override of the provider configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-import { routes } from './app/app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-
-const appConfig = {
-  providers: [provideRouter(routes)],
-};
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
