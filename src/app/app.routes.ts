16e31db75f3edc22e4778a2d2d7a73246a1d1c3f
nicov51 <nicolas.viennot@laposte.net>
2025-01-29 21:49:12 +0100

    💄 Add template user dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { PresentationComponent } from './components/pages/presentation/presentation.component';
 import { BilletterieComponent } from './components/pages/billetterie/billetterie.component';
 import { AuthComponent } from './components/pages/auth/auth.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'presentation', component: PresentationComponent },
   { path: 'billetterie', component: BilletterieComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'mesinfos', component: DashboardComponent },
 ];
