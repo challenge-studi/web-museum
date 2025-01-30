db8ca50a629798a25c700c36dc36eaddcb0e52c0
Rémi PONCHE <ponche62880@gmail.com>
2025-01-30 10:25:58 +0100

    👔 Create the service authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,61 @@
 import { Injectable } from '@angular/core';
+import User from '../models/UserInterface';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  private tokenJWT: string | undefined = undefined;
+
+  constructor(private http: HttpClient) {}
+
+  login(identifier: string, password: string) {
+    const observable = this.http
+      .post('/api/auth/local', {
+        identifier: identifier,
+        password: password,
+      })
+      .pipe(
+        tap((response) => {
+          if ('jwt' in response && typeof response.jwt === 'string') {
+            // Connexion Réussi:
+            this.tokenJWT = response.jwt;
+            console.log('Connexion Réussi');
+          } else {
+            console.error("Erreur d'authentification");
+            //TODO: implémenter la logique
+          }
+        }),
+      );
+
+    return observable;
+  }
+
+  logout() {
+    this.tokenJWT = undefined;
+
+    //TODO suppresion du localStorage ou du cookie et crée et mise a jour de observable User
+  }
+
+  register(user: User) {
+    //TODO: implémenter la fonction register
+    const observable = this.http.post('/api/auth/local/register', {
+      username: user.email,
+      email: user.email,
+      password: user.password,
+      birthday: user.birthday,
+      firstname: user.firstname,
+      lastname: user.lastname,
+    });
+
+    return observable;
+  }
+
+  getTokenJwt() {
+    return this.tokenJWT;
+  }
+
+  //TODO: implémenter un Observable user
 }
