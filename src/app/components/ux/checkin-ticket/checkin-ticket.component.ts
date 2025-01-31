aea6857d59c97fd9a0cc4a612852f84dbcaa212d
bhhdev <bhh.develop@gmail.com>
2025-01-31 22:35:51 +0100

    💄 Mofified component checkin-ticket and button

--- src/app/components/ux/checkin-ticket/checkin-ticket.component.ts
+++ src/app/components/ux/checkin-ticket/checkin-ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import Price from '../../../models/PriceInterface';
 
 @Component({
   selector: 'app-checkin-ticket',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './checkin-ticket.component.html',
   styleUrl: './checkin-ticket.component.css',
 })
-export class CheckinTicketComponent {}
+export class CheckinTicketComponent {
+  count = 0;
+  onValueChanged = output<{ id: number; count: number }>();
+
+  ticketType = input.required<Price>();
+
+  increment() {
+    this.count += 1;
+    this.onValueChanged.emit({ id: this.ticketType().id, count: this.count });
+  }
+  decrement() {
+    if (this.count > 0) this.count -= 1;
+  }
+}
--- src/app/models/PriceInterface.ts
+++ src/app/models/PriceInterface.ts
@@ -2,8 +2,4 @@ export default interface Price {
   id: number;
   tickets_type: string; // VARCHAR(15)
   price: number;
-
-  getPrice(): number;
-  setPrice(price: number): void;
-  getTicketsType(): string;
 }
