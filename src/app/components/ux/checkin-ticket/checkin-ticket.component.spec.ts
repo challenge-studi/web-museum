464e077cea0b73bdbef94059160eec94bd5ff872
bhhdev <bhh.develop@gmail.com>
2025-02-01 22:31:00 +0100

    ✅ Make 3 tests increment/decrement

--- src/app/components/ux/checkin-ticket/checkin-ticket.component.spec.ts
+++ src/app/components/ux/checkin-ticket/checkin-ticket.component.spec.ts
@@ -19,4 +19,21 @@ describe('CheckinTicketComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add 1 to the counter', () => {
+    component.increment();
+    expect(component.count).toBe(1);
+  });
+
+  it('should remove 1 from the counter', () => {
+    component.count = 5;
+    component.decrement();
+    expect(component.count).toBe(4);
+  });
+
+  it('should not be less than 0', () => {
+    component.count = 0;
+    component.decrement();
+    expect(component.count).toBe(0);
+  });
 });
