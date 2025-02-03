238bae949caed6d77f61be35803c297d081f63a6
nicov51 <nicolas.viennot@laposte.net>
2025-02-03 19:01:28 +0100

    🐛 Remove test button

--- src/app/services/command.service.spec.ts
+++ src/app/services/command.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CommandService } from './command.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CommandService', () => {
   let service: CommandService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(CommandService);
   });
 
